d2fdd1123982259837a85511d7b7d4bf6a7d5fa6
Gloria Guy <totallygloria@gmail.com>
2016-10-27 13:26:12 -0700

    The basic skeleton of a server file that imports the server declaration from a separate module and listens on the given port up. The http-server component is farming all of the CRUD operations out to separate modules, each of which is included. The modules for basic functionality have a dummy sanders call to the fs as a place holder until they're written out.

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 /*** Created by Gloria Anholt on 10/27/16. ***/
 
+const server = require('./lib/http-server');
+const port = process.env.PORT || 9999;
+
+server.listen(port, err => {
+  if (err) {
+    console.error('Server error: ', err);
+  } else {
+    console.log('HTPP Server listening on port ', port)
+  }
+});
