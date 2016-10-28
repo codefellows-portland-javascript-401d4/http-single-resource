133fcebc5c317e070fae00a9465a634b93190bf6
Gloria Guy <totallygloria@gmail.com>
2016-10-28 14:25:23 -0700

    Hand coded the expected results for querying the dummy data directory, requested a GET of /data folder, and checked if the dummy matches what is returned by the sanders fs call. Passes test!

--- test/server.test.js
+++ test/server.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const httpserver = require('../server');
+const httpserver = require('../lib/http-server');
 
 
 describe('Single-resource http server', () => {
@@ -12,12 +12,32 @@ describe('Single-resource http server', () => {
   it('hits the root and gets a 200 Ok', done => {
     server
       .get('/')
-      .end(function(err, res){
+      .end((err, res)=> {
         if (err) return done(err);
         assert.equal(res.status, 200);
         done();
       })
   });
+
+  it('retrieves all files in the data folder by name', done => {
+
+    let testHtml = '<h2>The data store contains the following files: </h2><ul>\n\n' +
+      '<li>boston.json</li>' +
+      '<li>los_angeles.json</li>' +
+      '<li>miami.json</li>' +
+      '<li>new_york.json</li>' +
+      '<li>san_francisco.json</li>' +
+      '</ul>' +
+      'To retrieve a file, please request by the file name (no extension)';
+
+    server
+      .get('/data')
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.equal(res.text, testHtml);
+        done();
+      })
+  })
 
 
 });
