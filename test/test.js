fe233682c80f136ac6ef5eb9ada6e6b5db6f5061
TomTimmel <tom.t.timmel@gmail.com>
2016-10-27 18:21:08 -0700

    added post functionality

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const assert = chai.assert;
+const server = require('../lib/http-server');
+
+describe('testing server.js', () =>{
+    let request = chai.request(server);
+
+    it('should serve up an index page', done =>{
+        request.get('/').end((err, res) =>{
+            if (err) return done (err);
+            assert.include(res.text, 'Welcome to our home page');
+            done();
+        });
+    });
+
+    it('should bring up a single file', done => {
+        request.get('/')
+    })
+
+
+
+});
