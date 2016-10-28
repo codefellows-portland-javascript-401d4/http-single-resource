f18d6246f690e67f4bd16e40795d1a5bdb562c53
gnkatchmar <gunk55@msn.com>
2016-10-27 22:59:43 -0700

    1027-2300 3 basic tests added

--- test/http-server-test.js
+++ test/http-server-test.js
@@ -1,41 +1,43 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
-const server = require("../lib/http-server");
+const server = require('../lib/http-server');
 
 let request = chai.request(server);
+//original two-team data resource displayed as text
+let origTeamsText = 'Oakland Athletics\nChicago Cubs\n';
 
-describe("http single resource server", () => {
+describe('http single resource server', () => {
 
-  it("error message on non-existent path", function (done) {
+  it('error message on non-existent path', function (done) {
     request
-            .get("/DoesNotExist")
+            .get('/DoesNotExist')
             .end((err, res) => {
               if (err) return done(err);
-              assert.equal(res.text, "404 - Not Found\nI told you there are only / and /Members in this rinky-dink website!");
+              assert.equal(res.text, '404 - Not Found');
               done();
             });
   });
 
-//   it("a path (/Members) correctly uses url.pathname", function (done) {
-//     request
-//             .get("/Members")
-//             .end((err, res) => {
-//               if (err) return done(err);
-//               assert.equal(res.text, membersRes);
-//               done();
-//             });
-//   });
+  it('a path (/teams) correctly uses url.pathname', function (done) {
+    request
+            .get('/teams')
+            .end((err, res) => {
+              if (err) return done(err);
+              assert.equal(res.text, origTeamsText);
+              done();
+            });
+  });
 
-//   it("/Members correctly uses queryData.name", function (done) {
-//     request
-//             .get("/Members?name=Greg+Katchmar")
-//             .end((err, res) => {
-//               if (err) return done(err);
-//               assert.equal(res.text, membersRes + "\nHello member Greg Katchmar!\n");
-//               done();
-//             });
-//   });
+  it('/teams correctly uses queryData.team', function (done) {
+    request
+            .get('/teams?team=Cubs')
+            .end((err, res) => {
+              if (err) return done(err);
+              assert.equal(res.text, 'Chicago Cubs');
+              done();
+            });
+  });
 
-// });
+});
