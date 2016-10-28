a1917f52a3f8e33a1ca26399c15a80ba8b203adc
Andrew Beers <andybeers@gmail.com>
2016-10-28 08:50:34 -0700

    Added first testing. Does not pass

--- test/http-test.js
+++ test/http-test.js
@@ -1,12 +1,39 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
 const server = require('../index.js');
+const rimraf = require('rimraf');
+const assert = chai.assert;
+chai.use(chaiHttp);
 
 describe('Our server responds to requests', done => {
-  let request = chai.request('http://localhost:8080');
-  it('should make a file from a POST request' done => {
-    request
-      .post('/')
-  })
-})
+
+  let locHost = 'http://localhost:8080';
+  let testTeam1 = {"teamName": "EHOME","teamMembers": "['test1', 'new chicken', 'RTZ', 'LaNm', 'Garder']","region": "CN","tiWinner": "false"}
+
+  before(done => {
+    rimraf('./lib/dotaTeams/*', err => {
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('should make a file from a POST request', done => {
+    chai.request(locHost)
+      .post('/teams')
+      .send(testTeam1)
+      .then(res => {
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.text, testTeam1);
+        done();
+      })
+      .catch(err => {
+        console.log('POST test 1 err');
+        throw err;
+      });
+  });
+
+  it('Should do this other thing', function() {
+
+  });
+
+});
