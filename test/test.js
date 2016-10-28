9882f729f41f3f294eeb3b4a357391bfbb8b001a
TomTimmel <tom.t.timmel@gmail.com>
2016-10-28 16:38:17 -0700

    fixed server and added a test

--- test/test.js
+++ test/test.js
@@ -3,11 +3,17 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const assert = chai.assert;
 const server = require('../lib/http-server');
+const sander = require('sander');
 
 describe('testing server.js', () =>{
     let request = chai.request(server);
+    const port = 8080;
 
-    it('should serve up an index page', done =>{
+    before(done => {
+        server.listen({port: port}, done);
+    });
+
+    it('serves up an index page', done =>{
         request.get('/').end((err, res) =>{
             if (err) return done (err);
             assert.include(res.text, 'Welcome to our home page');
@@ -15,7 +21,21 @@ describe('testing server.js', () =>{
         });
     });
 
-    // it('should bring up a single file', done => {
-    //     request.get('/')
-    // })
+    it('GETs a single item', done => {
+        request.get('/?1477695490435').end((err, res) => {
+            if (err) return done (err);
+            assert.include(res.text, '"hero":"Wonder Woman"');
+            done();
+        });
+    });
+
+    it.skip('POSTs an item and then DELETEs it', done => {
+        request.post().end((err, res) => {
+            console.log(res);
+            if (err) return done(err);
+            sander.readdir('../lib/data-store')
+            .then(assert.include(res.text, ''));
+            done();
+        });
+    });
 });
