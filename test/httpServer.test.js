89387e78bcf8258f09e6668661e36a3d088e1eeb
Christopher Bruner <chrbruner9@gmail.com>
2016-10-28 15:18:42 -0700

    refactored post, get, and delte handlers and refactored response from server to become more modular

--- test/httpServer.test.js
+++ test/httpServer.test.js
@@ -15,7 +15,7 @@ describe('http single resource promise server', () => {
         server.listen({port, port}, done);
     });
 
-    it('checks to see that we can retrieve a given cat', done => {
+    it('checks to see that we can retrieve a given cat with GET', done => {
         request
             .get('/cats/1')
             .end((err, res) => {
