0bc7d60fd9be14727a01df82ca2ce7d9a36b0633
Christopher Bruner <chrbruner9@gmail.com>
2016-10-29 10:29:09 -0700

    refactored all handlers and server to make them clearer, cleaned up unnecessary code

--- test/httpServer.test.js
+++ test/httpServer.test.js
@@ -1,10 +1,10 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai      = require('chai');
+const chaiHttp  = require('chai-http');
+const assert    = chai.assert;
+const server    = require('../lib/httpServer');
+const fs        = require('fs');
+const sander    = require('sander');
 chai.use(chaiHttp);
-const assert = chai.assert;
-const server = require('../lib/httpServer');
-const fs = require('fs');
-const sander = require('sander');
 
 describe('http single resource promise server', () => {
     
