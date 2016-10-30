92e23752a27a9aac485d32ebb977add20aaad277
Steven Jackson <Steven Jackson>
2016-10-30 15:51:36 -0700

    have gotten the data out and am able to console.log the data. Having a problem getting that data to display in postman

--- run-http.js
+++ run-http.js
@@ -1,6 +1,6 @@
 const server = require('./lib/http-server');
 
-const port = 9000;
+const port = 8080;
 server.listen(port, err => {
     if (err) console.log('Error', err);
     else console.log('http server listening on port ', port);
