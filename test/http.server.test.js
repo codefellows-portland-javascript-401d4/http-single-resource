8839f5600e6ffa8c481371129da0f0416eb69353
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-01 13:24:52 -0700

    Adding teams data and initial testing

--- test/http.server.test.js
+++ test/http.server.test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = require('chai').expect;
 const assert = require('chai').assert;
-const server = require('../lib/http-server');
+const server = require('../lib/football-server');
 const port = 8080;
 
 describe('server', () => {
@@ -12,16 +12,28 @@ describe('server', () => {
     });
 });
 
-describe('test http server', () => {
+describe('test http server resource', () => {
     let request = chai.request(server);
 
     it('sends back a response text', done => {
         request
-            .get('/')
+            .get('/teams')
             .end((err, response) => {
                 if(err) return done(err);
-                assert.deepEqual(response.text, )
-            })
-    })
+                assert.deepEqual(response.text, 'broncos.json ' + 'seahawks.json ');
+                expect(response).to.have.status(200);
+                done();
+            });
+    });
 
-})
+    it('request for resource in /teams', done => {
+        request
+          .get('/teams/seahawks')
+          .end((err, response) => {
+              if(err) return done(err);
+              assert.deepEqual(response.text, '{"city":"seattle","conference":"nfc"}\n');
+              expect(response).to.have.status(200);
+              done();
+          });
+    });
+});
